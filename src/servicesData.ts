323091e444e345e5f1f5ceb36949e5f9a7969e98
Kai Makana <willwisn11@gmail.com>
2026-05-31 17:31:49 -0500

    refactor: update asset import paths
    Move image assets to the root directory for better organization and consistent import paths across components.

--- src/servicesData.ts
+++ src/servicesData.ts
@@ -1,13 +1,13 @@
 import { ServiceDetail, GalleryItem, SeoLandingPage } from "./types";
 
-import porchBefore from "./PorchBefore1.jpg";
-import porchAfter from "./PorchAfter1.jpg";
-import concreteBefore from "./Concretebefore.jpg";
-import concreteAfter from "./Concreteafter.jpg";
-import poolBefore from "./Poolbefore.jpg";
-import poolAfter from "./poolafter.jpg";
-import divingBefore from "./DivingBefore1.jpg";
-import divingAfter from "./DivingAfter1.jpg";
+import porchBefore from "../PorchBefore1.jpg";
+import porchAfter from "../PorchAfter1.jpg";
+import concreteBefore from "../Concretebefore.jpg";
+import concreteAfter from "../Concreteafter.jpg";
+import poolBefore from "../Poolbefore.jpg";
+import poolAfter from "../poolafter.jpg";
+import divingBefore from "../DivingBefore1.jpg";
+import divingAfter from "../DivingAfter1.jpg";
 
 export const SERVICES_DATA: ServiceDetail[] = [
   {
